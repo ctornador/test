57ad59e360922bdb0422d3f286367edb557058ed
ctornador <cristian.tornador@gmail.com>
2016-01-18 13:03:12 +0100

    write

---
+++ test/helloworld/hello.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package helloworld;
+
+/**
+ * @author Cristian
+ *
+ */
+public class hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+	}
+
+}
