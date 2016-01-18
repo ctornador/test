eac7e6325dc0f55a6febba9a85e10fbe168cc868
ctornador <cristian.tornador@gmail.com>
2016-01-18 12:53:37 +0100

    first change

---
+++ test/helloworld/Hi.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package helloworld;
+
+/**
+ * @author Cristian
+ *
+ */
+public class Hi {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
